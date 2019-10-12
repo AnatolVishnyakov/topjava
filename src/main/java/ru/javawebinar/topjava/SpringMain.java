66b28eac8dfd1c60261c945429bdc23ecb29b292
Anatol V <malfenius@gmail.com>
2019-10-12 20:26:46 +0300

    HW-2. Task-1. Implements InMemoryUserRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -5,23 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.web.user.AdminRestController;
-import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
 public class SpringMain {
     public static void main(String[] args) {
-/*        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-
-//        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
-        UserRepository userRepository = appCtx.getBean(UserRepository.class);
-        userRepository.getAll();
-
-        UserService userService = appCtx.getBean(UserService.class);
-        userService.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-
-        appCtx.close();*/
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
---
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryUserRepository.java
@@ -0,0 +1,55 @@
+package ru.javawebinar.topjava.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return repository.remove(id) != null;
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("save {}", user);
+        return repository.putIfAbsent(user.getId(), user);
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        return repository.values()
+                .stream()
+                .sorted(Comparator.comparing(User::getName))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        return repository.values()
+                .stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
+    }
+}
