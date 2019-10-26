82660a23bfbd89f8af104b08fb431cd4bdf7dfb3
Anatol V <malfenius@gmail.com>
2019-10-26 18:35:22 +0300

    HW-2.3. Refactoring isBetween on generic type

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.util;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
@@ -19,11 +18,7 @@ public class DateTimeUtil {
                 ? "" : localDateTime.format(DEFAULT_DATE_FORMAT);
     }
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
-    }
-
-    public static boolean isBetween(LocalDate lt, LocalDate startDate, LocalDate endDate) {
-        return lt.compareTo(startDate) >= 0 && lt.compareTo(endDate) <= 0;
+    public static <T extends Comparable<? super T>> boolean isBetween(T value, T start, T end) {
+        return value.compareTo(start) >= 0 && value.compareTo(end) <= 0;
     }
 }
