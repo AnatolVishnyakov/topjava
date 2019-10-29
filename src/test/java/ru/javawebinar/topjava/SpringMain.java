f36b866fbd626e715f618f2848fe369ec9c48580
Anatol V <malfenius@gmail.com>
2019-10-29 21:22:22 +0300

    HW-3.5 Add SpringRunner test

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,15 +1,5 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.web.meal.MealRestController;
-import ru.javawebinar.topjava.web.user.AdminRestController;
-
-import java.time.LocalDateTime;
-import java.util.Arrays;
-
 public class SpringMain {
     public static void main(String[] args) {
 //        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
