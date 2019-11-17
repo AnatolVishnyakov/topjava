64638962ecf10c6ace26d22ebbc09d8346a0ea56
Anatol V <malfenius@gmail.com>
2019-11-17 21:51:05 +0300

    HW-3. Init meal data

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -13,9 +13,12 @@ CREATE TABLE users
   password         VARCHAR                 NOT NULL,
   registered       TIMESTAMP DEFAULT now() NOT NULL,
   enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+  calories_per_day INTEGER DEFAULT 2000    NOT NULL,
+  meal_id          INTEGER not null,
+  FOREIGN KEY (meal_id) REFERENCES meals (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+    ON users (email);
 
 CREATE TABLE user_roles
 (
@@ -25,10 +28,14 @@ CREATE TABLE user_roles
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE meals
-(
-    id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    datetime        TIMESTAMP           NOT NULL,
-    description     VARCHAR             NOT NULL,
-    calories        INTEGER             NOT NULL
+CREATE TABLE meals (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id     INTEGER   NOT NULL,
+  date_time   TIMESTAMP NOT NULL,
+  description TEXT      NOT NULL,
+  calories    INT       NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX meals_unique_user_datetime_idx
+    ON meals (user_id, date_time);
