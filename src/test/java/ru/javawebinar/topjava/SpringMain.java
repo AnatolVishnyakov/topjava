6691d15d5c1bc1cae821f3aebc3f5062fbbf8f72
Anatol V <malfenius@gmail.com>
2019-11-17 21:51:32 +0300

    HW-3. Fix test SpringMain

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,17 +1,38 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.web.meal.MealRestController;
+import ru.javawebinar.topjava.web.user.AdminRestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
 public class SpringMain {
     public static void main(String[] args) {
-//        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-//        AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-//        adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-//        System.out.println();
-//
-//        MealRestController mealController = appCtx.getBean(MealRestController.class);
-//        List<MealTo> filteredMealsWithExcess =
-//                mealController.getBetween(
-//                        LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-//                        LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-//        filteredMealsWithExcess.forEach(System.out::println);
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+        MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+
+        mealRestController.create(new Meal(null, LocalDateTime.now(), "test", 2_000));
+        adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+
+        System.out.println();
+
+        MealRestController mealController = appCtx.getBean(MealRestController.class);
+        List<MealTo> filteredMealsWithExcess =
+                mealController.getBetween(
+                        LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                        LocalDate.of(2020, Month.MAY, 31), LocalTime.of(11, 0));
+        filteredMealsWithExcess.forEach(System.out::println);
     }
 }
