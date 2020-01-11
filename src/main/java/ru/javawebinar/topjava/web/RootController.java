bccb5cb987ff11d1e617ec33da11da8146102646
Anatol V <malfenius@gmail.com>
2020-01-11 15:39:32 +0300

    HW-6.14. Spring web. Add mapping controller

---
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RootController {
+    private final UserService service;
+
+    public RootController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:meals";
+    }
+}
